131a6cf3871a1f91496e4dd3ad8c90b63d3ca593
Tinker <wahhabzendehdel@gmail.com>
2023-12-18 12:15:06 +0330

    git init

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from git import Repo
-
+import os
 # Initialize a new repo in the current directory
 repo = Repo.init()
-
-repo.config_writer().set_value("user.email", "Wahhabzendehdel@gmail.com").release()
-repo.config_writer().set_value("user.email", "Wahhabzendehdel@gmail.com").release()
-
-print(repo.config_reader().get_value("user.email"))
+# repo.config_writer().set_value('user', 'email', 'Wahhabzendehdel@gmail.com')
+username = 'Wahhab-Zendehdel'
+os.environ['GIT_API_KEY'] = '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'
 # Add remote origin
-origin = repo.create_remote('origin', 'https://github.com/Wahhab-Zendehdel/test.git')
+origin = repo.create_remote('origin', f'https://{username}:{os.environ["GIT_API_KEY"]}@github.com/Wahhab-Zendehdel/test.git')
 
 # Add file to the staging area
 repo.index.add(['main.py'])
